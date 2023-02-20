88d87aced055b743dc602e4357c5f2a17168f7e6
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-02-20 22:59:37 +0530

    add background to item view and border to image and included styles

--- app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
+++ app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
@@ -13,10 +13,10 @@ public class ItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item);
         Bundle bundle=getIntent().getExtras();
         ((TextView)findViewById(R.id.item_name)).setText(bundle.getString("name"));
-        ((TextView)findViewById(R.id.material_type)).setText(bundle.getString("material"));
-        ((TextView)findViewById(R.id.closest_hub)).setText(bundle.getString("closestHub"));
-        ((TextView)findViewById(R.id.alternative_use)).setText(bundle.getString("alternativeUse"));
-        ((TextView)findViewById(R.id.local_resources)).setText(bundle.getString("localResources"));
-        ((TextView)findViewById(R.id.recycleable)).setText(String.valueOf(bundle.getBoolean("recycleable")));
+        ((TextView)findViewById(R.id.material_type)).setText(String.format("Material: %s", bundle.getString("material")));
+        ((TextView)findViewById(R.id.closest_hub)).setText(String.format("Closest hub for disposal:\n%s", bundle.getString("closestHub")));
+        ((TextView)findViewById(R.id.alternative_use)).setText(String.format("Alternative use for item:\n%s", bundle.getString("alternativeUse")));
+        ((TextView)findViewById(R.id.local_resources)).setText(String.format("Locally available resources:\n%s", bundle.getString("localResources")));
+        ((TextView)findViewById(R.id.recycleable)).setText(String.format("Is the item recyclable: %s", bundle.getBoolean("recycleable")));
     }
 }
