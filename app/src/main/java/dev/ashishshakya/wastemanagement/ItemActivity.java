784df5cd5e54af51b4ab3d7a7cbf39662a4594a5
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-02-21 12:25:23 +0530

    add default image for when user does not upload image

--- app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
+++ app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
@@ -2,14 +2,18 @@ package dev.ashishshakya.wastemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Objects;
+
 public class ItemActivity extends AppCompatActivity {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,10 @@ public class ItemActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.alternative_use)).setText(String.format("Alternative use for item:\n%s", bundle.getString("alternativeUse")));
         ((TextView)findViewById(R.id.local_resources)).setText(String.format("Locally available resources:\n%s", bundle.getString("localResources")));
         ((TextView)findViewById(R.id.recycleable)).setText(String.format("Is the item recyclable: %s", bundle.getBoolean("recycleable")));
-        Glide.with(this).load(bundle.getString("imageUrl")).into((ImageView) findViewById(R.id.item_image));
-
-
+        if(Objects.equals(bundle.getString("imageUrl"), "")){
+            ((ImageView) findViewById(R.id.item_image)).setImageDrawable(getDrawable(R.drawable.image_not_found));
+        }else{
+            Glide.with(this).load(bundle.getString("imageUrl")).into((ImageView) findViewById(R.id.item_image));
+        }
     }
 }
