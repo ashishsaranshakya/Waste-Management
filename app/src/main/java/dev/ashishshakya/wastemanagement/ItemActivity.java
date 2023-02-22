134f88068700a98d72d68d224594e991dab1656f
Ashish Saran Shakya <harshshakya18585@gmail.com>
2023-02-22 22:59:20 +0530

    change item view to be consistent with all ui theme

--- app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
+++ app/src/main/java/dev/ashishshakya/wastemanagement/ItemActivity.java
@@ -24,7 +24,8 @@ public class ItemActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.closest_hub)).setText(String.format("Closest hub for disposal:\n%s", bundle.getString("closestHub")));
         ((TextView)findViewById(R.id.alternative_use)).setText(String.format("Alternative use for item:\n%s", bundle.getString("alternativeUse")));
         ((TextView)findViewById(R.id.local_resources)).setText(String.format("Locally available resources:\n%s", bundle.getString("localResources")));
-        ((TextView)findViewById(R.id.recycleable)).setText(String.format("Is the item recyclable: %s", bundle.getBoolean("recycleable")));
+        if (bundle.getBoolean("recycleable")) ((TextView)findViewById(R.id.recycleable)).setText(new String("Is the item recyclable: Yes"));
+        else ((TextView)findViewById(R.id.recycleable)).setText(new String("Is the item recyclable: No"));
         if(Objects.equals(bundle.getString("imageUrl"), "")){
             ((ImageView) findViewById(R.id.item_image)).setImageDrawable(getDrawable(R.drawable.image_not_found));
         }else{
